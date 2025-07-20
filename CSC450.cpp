f8870bdc6aca17e88f8b8b2a51a1dc9901622818
Raishawn Jones <141539039+LLTS1022@users.noreply.github.com>
2025-07-20 17:38:58 -0500

    Add files via upload
    Final Submission, first commit

---
+++ CSC450.cpp
@@ -0,0 +1,69 @@
+//============================================================================
+// Name        : CSC450.cpp
+// Author      : Raishawn Jones
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Module 1 Critical Thinking Assignment
+//============================================================================
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Person's information
+    string firstName = "Sam";
+    string lastName = "Malone";
+    string streetAddress = "112 1/2 Beacon Street";
+    string city = "Boston";
+    string zipCode = "02108";
+
+    // Display the above Person's information
+    cout << "Person's Information:" << endl;
+    cout << "First Name: " << firstName << endl;
+    cout << "Last Name: " << lastName << endl;
+    cout << "Street Address: " << streetAddress << endl;
+    cout << "City: " << city << endl;
+    cout << "Zip Code: " << zipCode << endl;
+
+    return 0;
+}
+
+
+/*//Simple Program with a few Errors for Correction
+//Please be sure to correct all outlined errors.
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+//Main Function
+int main()
+{
+//Standard Output Statement
+cout<<"Welcome to this C++ Program"<<endl;
+cout<< "I have corrected all errors for this program." <<endl;
+// Wait For Output Screen
+//Main Function return Statement
+} */
+
+
+/* #include<iostream>
+#include<conio.h>
+//Standard namespace declaration
+using namespace std;
+//Main Function
+int main()
+{
+	 double myMoney = 1000.50; //this should be printed out
+//Standard Output Statement
+cout << "Please be sure to correct all syntax errors in this program" << endl;
+cout << "I have corrected all errors for this program." << endl;
+cout << "The total amount of money available is = " << myMoney << endl;
+// Wait For Output Screen
+//Main Function return Statement
+return 0;
+}*/
+
+
+
+
